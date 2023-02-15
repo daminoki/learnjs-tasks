fd7b7ea273f705c5ffc67b1e8e58a0f7883af27b
Daria Andreeva <dd1556@mail.ru>
2023-02-15 18:36:31 +0300

    completed interactions chapter

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import { sayHi } from './javascript-fundamentals/hello-world/show-an-alert.js';
+import { sayYourName } from './javascript-fundamentals/interaction-alert-prompt-confirm/a-simple-page.js';
 
-// sayHi();
+sayYourName();
---
+++ javascript-fundamentals/interaction-alert-prompt-confirm/a-simple-page.js
@@ -0,0 +1,4 @@
+export function sayYourName() {
+    let name = prompt('What is your name?', '');
+    alert(`Your name is ${name}!`);
+}
